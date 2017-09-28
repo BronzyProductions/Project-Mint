94ca573b5d1966996604e6a719ac9cda372bc4aa
Bronzy <BronzyProductions@users.noreply.github.com>
2017-09-28 09:23:18 +0100

    Create MadLib.py

---
+++ MadLib.py
@@ -0,0 +1,15 @@
+#MadLib Advanced Python
+sillyWord1 = (input("Enter a silly word..."))
+lastName = (input("Enter your last name..."))
+illness = (input("Enter an illness..."))
+nounPlural = (input("Enter a noun (plural)..."))
+adjective1 = (input("Enter an adjective..."))
+adjective2 = (input("Enter another adjective..."))
+sillWord2 = (input("Enter another silly word..."))
+place = (input("Enter a place..."))
+number = int(input("Enter a number..."))
+adjective3 = (input("Enter another adjective..."))
+print(sillyWord1, lastName, "will not be attending school today as he/she has come down with a case of",
+illness, "and has horrible", nounPlural, "and a/an", adjective1, "fever. We have made an appointment with
+the", adjective2, "Dr", sillWord2, "who studied for many years in", place, "and has", number, "degrees in
+pediatrics. He will send you all the information you need. Thank you!, Sincerely Mrs", adjective3)
