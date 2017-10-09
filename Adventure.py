793d8c71b43ce2dd6e1d04b6ae9cc026ed41c6f3
Bronzy <BronzyProductions@users.noreply.github.com>
2017-10-09 15:01:39 +0100

    Create Adventure.py

---
+++ Adventure.py
@@ -0,0 +1,32 @@
+#Adventure Game in Python
+from time import sleep
+from datetime import datetime
+print("Welcome to Bronzy a BronzyProductions Game, please ensure all choices start with a capital letter and that spelling is correct or your choice will result in an error, thanks for playing.")
+sleep(3)
+#Welcome to the busy city of Cybro
+print("Welcome to Cybro city the bustling capital of the planet Zelot, Zelot being just one of 25 of the planets with the highest occupation of human life. It ranks in at number 3, Mars is 1, and Euphoria a new moon of Saturn is number 2. Earth was discarded by all its life, now it serves a maximum security prison.")
+sleep(10)
+print("Even though the capital has extreme wealth it still uses hover transportation, on the hoverTram you look at yourself in the reflection of the reflection of your laptop, people are staring at you because that laptop is literally from the 21st century, a galactic Sahr comes over to you and insults you on your laptop stating that its the 26th century, Sahrs have always been mean well ever since Elon Musk kicked them of Mars.")
+sleep(10)
+print("Sorry this monologue is taking ages and its nearly your stop. Time to decide on how your gonna be seen by the over 18654 species of alien living on planet Zelot.")
+sleep(10)
+
+name=input("What is your name?...")
+gender=input("Are you a male or female?...")
+age=input("What age are you?...")
+height=input("How tall are you, in feet?...")
+hair=input("What is your hair colour?...")
+eyes=input("What colour is your eyes?...")
+sleep(1)
+
+print("Ok not like I already knew all that anyway I set you up to meet with a friend, he’ll set you up. Congratulations on the move but remember as great as the capital is there is danger at every turn so make sure to be prepared, you can buy cheap laser cannons on the black market you shoukd go there when your settled. Oh my names Ava by the way i’ll pop up every now and then")
+sleep(10)
+print("...........................................................................................................")
+print("ACHIEVEMENT UNLOCKED:  WELCOME TO THE CITY")
+print("...........................................................................................................")
+sleep(5)
+
+print("You hear a voice chanting your name and follow the high pitched chant, that must the guy that Ava set up to show you the way around, you should say hi.")
+greeting=input("Say hello to the man...")
+print("Oh hi, you must be", name)
+print("Do you say Yes or Stare at his third eye")
