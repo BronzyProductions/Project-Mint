632399a7712870b59cb464529cb6eef02323fbba
Bronzy <BronzyProductions@users.noreply.github.com>
2017-10-26 09:41:51 +0100

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,19 @@
+#Making Lists
+list1 = ['physics', 'chemistry', 1977, 2000];
+list2 = [1, 2, 3, 4, 5 ];
+list3 = ["a", "b", "c", "d"]
+
+#Accessing Lists
+print(list1)
+
+#Updating Lists
+list1.append('history')
+print(list1)
+
+#Inserting Lists (Can assign the value to a certain area in list)
+list1.insert(0, 'science')
+print(list1)
+
+#Removing In lists
+list1.remove(2)
+print(list1)
