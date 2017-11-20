6b8195285263ed61d71f96f9b619d0e8ecdf2328
Bronzy <BronzyProductions@users.noreply.github.com>
2017-11-20 15:16:21 +0000

    Create game.py

---
+++ game.py
@@ -0,0 +1,17 @@
+#Daniel + Steven
+#Welcome To The City
+from time import sleep
+print("Welocome to 'Welcome To The City'")
+sleep(2)
+print("You start on a hover train, on your way to the world famous Cybro City the capitol of Zelot")
+sleep(2)
+while True:
+    choice=input("Before you start would you like to start a tutorial y/n")
+    sleep(3)
+    if choice=="n":
+        print("Okay your ready to get started, if you ever get confused simply just come back and run a refresher course")
+        sleep(4)
+        break
+    elif choice=="y":
+        print("OKAY prepare for a story and a half!!")
+        sleep(2)
