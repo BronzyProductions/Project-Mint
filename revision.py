2595b7c2a49210014ec35e0adb448060d631428c
Bronzy <BronzyProductions@users.noreply.github.com>
2018-01-15 15:03:43 +0000

    Create revision.py

---
+++ revision.py
@@ -0,0 +1,9 @@
+#Welcome To my Programme
+
+#1: Print:
+print("Hello, this is a print statement")
+
+#2: Sleep:
+from time import sleep
+sleep(28800)
+print("The Sleep element delays a piece of code from going through")
