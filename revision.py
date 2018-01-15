14269d72bdf0aa3e3f4a38c8684ebbb2003b69aa
Bronzy <BronzyProductions@users.noreply.github.com>
2018-01-15 15:32:13 +0000

    Update revision.py

--- revision.py
+++ revision.py
@@ -7,3 +7,27 @@ print("Hello, this is a print statement")
 from time import sleep
 sleep(28800)
 print("The Sleep element delays a piece of code from going through")
+sleep(5)
+
+#3: Input:
+name = input("What's your name")
+print(name, "nice name")
+sleep(5)
+
+#4: While Loop:
+word = "computer"
+for letter in word:
+    print letter
+sleep(5)
+
+#5: If Else
+choice==input("Is your name",name)
+if choice==(name)
+print("Hello", name)
+
+else:
+    break
+
+#6: Functions:
+def dead():
+    print("Goodbye")
