7025b8cc4ecb665810e3b9e16e8533e791c4124b
Bronzy <BronzyProductions@users.noreply.github.com>
2020-10-12 09:18:15 +0100

    Create forloopsnumber.py

---
+++ forloopsnumber.py
@@ -0,0 +1,12 @@
+# Stephen Flynn Revised Loop Counter For Numbers
+# Works by using the X unknown variable and putting it into the range of numbers to print. By using a print statement printing the numbers stored in that variable.
+for x in range(5):
+    print(x)
+
+# Prints out 3,4,5
+for x in range(3, 6):
+    print(x)
+
+# Prints out 3,5,7
+for x in range(3, 8, 2):
+    print(x)
